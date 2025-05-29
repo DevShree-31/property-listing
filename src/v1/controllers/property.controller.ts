8c7ba38b86c3dd2f5346a7f21bae52906d4a4d13
Shreyansh <patelshreyansh376@gmail.com>
2025-05-30 00:45:58 +0530

    feat: add getPropertyById function and corresponding route with Swagger documentation

--- src/v1/controllers/property.controller.ts
+++ src/v1/controllers/property.controller.ts
@@ -1,22 +1,23 @@
-import {  Response } from "express";
+import { Response } from "express";
 import { AuthRequest } from "../../types/auth/authTypes";
 import { UserModel } from "../models/user.model";
 import { sendError, sendSuccess } from "../../utils/helper";
 import { StatusCodes } from "http-status-codes";
 import { IProperty, PropertyModel } from "../models/property.model";
 import { logger } from "../../utils/logger";
- async function createProperty(req: AuthRequest, res: Response) {
+import mongoose from "mongoose";
+async function createProperty(req: AuthRequest, res: Response) {
     try {
         console.log("Entered create property function")
         const { _id } = req.user
         const propertyDetails = req.body
         const user = await UserModel.findById({ _id })
-        console.log("Details of the property",propertyDetails)
+        console.log("Details of the property", propertyDetails)
         if (!user) {
             return sendError(res, "User not found", StatusCodes.NOT_FOUND)
         }
 
-        const property:IProperty = await PropertyModel.create({ ...propertyDetails, createdBy: user._id })
+        const property: IProperty = await PropertyModel.create({ ...propertyDetails, createdBy: user._id })
         console.log(property)
         return sendSuccess(res, "Property Created Successfully", StatusCodes.CREATED, property)
     } catch (error) {
@@ -25,8 +26,21 @@ import { logger } from "../../utils/logger";
     }
 }
 
+async function getPropertyById(req: AuthRequest, res: Response) {
+    try {
+        const id = new mongoose.Types.ObjectId(req.params.id);
+        const property = await PropertyModel.findById({ _id: id })
 
+        if (!property) {
+            return sendError(res, "Property not found", StatusCodes.NOT_FOUND)
+        }
 
+        return sendSuccess(res, "Property with the given id retrieved successfully", StatusCodes.OK, property)
+    } catch (error) {
+        return sendError(res, "Error occured while retrieving the property", StatusCodes.INTERNAL_SERVER_ERROR, error)
+    }
 
-export {createProperty}
+}
+
+export { createProperty, getPropertyById }
 
--- src/v1/routes/property.route.ts
+++ src/v1/routes/property.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { validateProperty } from "../../middlewares/property.validation.middleware";
 import { jwtAuthMiddleware } from "../../middlewares/jwt.auth.middleware";
-import { createProperty } from "../controllers/property.controller";
+import { createProperty, getPropertyById } from "../controllers/property.controller";
 
 const router =Router()
 /**
@@ -27,4 +27,28 @@ const router =Router()
  *         description: Bad request
  */
 router.post('/', validateProperty,jwtAuthMiddleware,createProperty );
+/**
+ * @swagger
+ * /api/v1/property/{id}:
+ *   get:
+ *     summary: Get a property by id
+ *     tags: [Property]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The unique identifier of the property
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Property retrieved successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Property'
+ *       400:
+ *         description: Bad request
+ */
+router.get('/:id',jwtAuthMiddleware,getPropertyById)
 export default router
